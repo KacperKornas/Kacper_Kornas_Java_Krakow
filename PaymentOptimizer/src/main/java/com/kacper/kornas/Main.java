67b9a4521c9b1291416746921ee37c5713dbb45f
KacperKornas <kornaskacper1@gmail.com>
2025-05-10 17:43:29 +0200

    try to read json

--- PaymentOptimizer/src/main/java/com/kacper/kornas/Main.java
+++ PaymentOptimizer/src/main/java/com/kacper/kornas/Main.java
@@ -1,6 +1,9 @@
 package com.kacper.kornas;
 
+import com.google.gson.Gson;
+import java.io.FileReader;
 import java.util.*;
+
 
 public class Main {
 
@@ -9,6 +12,7 @@ public class Main {
         String value;
         List<String> promotions;
     }
+
     static class PaymentMethodRaw {
         String id;
         String discount;
@@ -20,13 +24,24 @@ public class Main {
         double value;
         List<String> promotions;
     }
+
     static class PaymentMethod {
         String id;
         double discount;
         double limit;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.out.println("Usage: java com.kacper.kornas.Main orders.json paymentmethods.json");
+            return;
+        }
 
+        String ordersPath = "data/" + args[0];
+        String methodsPath = "data/" + args[1];
+
+        Gson gson = new Gson();
+        OrderRaw[] rawOrders = gson.fromJson(new FileReader(ordersPath), OrderRaw[].class);
+        PaymentMethodRaw[] rawMethods = gson.fromJson(new FileReader(methodsPath), PaymentMethodRaw[].class);
     }
 }
